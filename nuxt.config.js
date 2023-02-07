6026c104d05dd2cea726a475a07813d3cc879f24
SuelSaraci <suelsaraqi1@gmail.com>
2023-02-07 16:08:36 +0100

    X

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,7 @@ export default {
   css: ['@/assets/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~/plugins/gtm.js', mode: 'client' }],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
--- plugins/gtm.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import VueGtm from 'vue-gtm'
-
-Vue.use(VueGtm, {
-  id: 'GTM-5D3N6QM', // your GTM tracking ID
-})
